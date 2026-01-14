4fde9584da4e8ddd03773568cd1a32f12795ec22
cedriclyh <cedriclim2001@gmail.com>
2026-01-14 18:42:34 +0800

    added functions to userController

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,7 +1,10 @@
 import db from '../config/db.js';
 
+
+// READ (all users - without password)
 export const getUsers = async (req, res) => {
     try {
+        // TODO: need to change select columns
         const [rows] = await db.query('SELECT * FROM users');
         res.status(200).json(rows);
     }
@@ -10,3 +13,20 @@ export const getUsers = async (req, res) => {
         res.status(500).json({ error:'Internal Server Error'});
     }
 };
+
+// READ (all users - filter by deactivated)
+
+// READ (Single User)
+export const getUserById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const [rows] = await db.query('SELECT * FROM users WHERE user_id = ?', [id]);
+
+        if (rows.length === 0) return res.status(404).json({ message: "User not found." });
+
+        res.status(200).json(rows[0]);
+    }
+    catch (error) {
+        res.status(500).json({ error:'Internal Server Error'});
+    }
+}
