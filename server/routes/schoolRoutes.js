8be8ae1a5bf0a1c1e37bedcdffa5048173855431
cedriclyh <cedriclim2001@gmail.com>
2026-01-17 16:48:02 +0800

    commented api for schools

--- server/routes/schoolRoutes.js
+++ server/routes/schoolRoutes.js
@@ -3,6 +3,6 @@ import * as schoolController from '../controllers/schoolController.js';
 
 const router = express.Router();
 
-router.get('/', getAllSchools); // GET /api/schools
+// router.get('/', getAllSchools); // GET /api/schools
 
 export default router;
