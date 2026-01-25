f596b5acfbd8b62a6958865f647e613e75dd9d75
mynameisbacon88 <cedriclim2001@gmail.com>
2026-01-26 01:23:09 +0800

    school API code rearrangement

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import userRoutes from './userRoutes.js';
-import schoolRoutes from './schoolRoutes.js';
+import schoolRoutes from './school.routes.js';
 import surveyRoutes from './survey.routes.js';
 import responsesRoutes from './responses.routes.js';
 
@@ -13,7 +13,6 @@ apiRouter.use(cors()); // Allows your Next.js frontend to talk to this server
 apiRouter.use(express.json()); // Allows the server to read req.body from your form
 
 // Route Group: /api/users
-// This matches your goal of having users/...
 apiRouter.use('/users', userRoutes);
 
 // Route Group: /api/schools
