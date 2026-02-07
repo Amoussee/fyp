1d75460741bedd7a5587bf23bb2fdf17bd50b358
SandraTan08 <sandra.tan.2022@scis.smu.edu.sg>
2026-02-07 14:27:35 +0800

    solve import issues

--- tcc/src/app/(authed)/admin/survey-toolkit/surveys/page.tsx
+++ tcc/src/app/(authed)/admin/survey-toolkit/surveys/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useMemo } from 'react';
+import { useState, useMemo, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { SurveyList, type Survey } from '../../../../../components/ui/surveyList';
 import { SurveyTemplateList } from '../../../../../components/ui/surveyTemplateList';
